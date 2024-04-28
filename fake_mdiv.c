15bfdcf746bfced75b2d6bba9ae02cb602c9d123
Konrad Kaczmarczyk <kk459668@students.mimuw.edu.pl>
2024-04-28 17:08:47 +0200

    done, but not documented

--- fake_mdiv.c
+++ fake_mdiv.c
@@ -4,17 +4,24 @@
 
 int64_t mdiv(int64_t *x, size_t n, int64_t y){
     int64_t sign = x[--n] ^ y;
-    int64_t buf = (x[n] % y + y) % y;
-    x[n] = (x[n] - buf) / y;
+    int64_t buf;
+    if(x[n] == INT64_MIN && y == -1){
+        buf = -1;
+        x[n] = INT64_MAX;
+    } else {
+        buf = (x[n] % y + y) % y;
+        x[n] = (x[n] - buf) / y;
+    }
     while(n > 0){
         n--;
-        for(char j = 8 * sizeof(int64_t) - 1 ; j >= 0; j--){
+        for(int64_t j = 8 * sizeof(int64_t) - 1; j >= 0; j--){
             buf = (buf << 1) + ((((uint64_t) x[n]) >> j) & 1ULL);
-            if(buf / y)
-                x[n] |= (1ULL << j);
-            else
+            if((buf ^ (buf - y)) < 0 && buf != y ){
                 x[n] &= ~(1ULL << j);
-            buf %= y;
+            } else {
+                x[n] |= (1ULL << j);
+                buf -= y;
+            }
         }
     }
     if(sign < 0 && buf != 0){
@@ -23,5 +30,8 @@ int64_t mdiv(int64_t *x, size_t n, int64_t y){
             x[n]++;
         }while(x[n++] == 0);
     }
+    if(buf == y)
+        raise(SIGFPE);
+
     return buf;
 }
--- mdiv.asm
+++ mdiv.asm
@@ -1,74 +1,97 @@
 global mdiv
 mdiv:
-	mov	r11, rdi
-	lea	rdi, -1[rsi]
 	push	rbp
-	mov	r8, rdx
-	lea	r9, 0[0+rdi*8]
 	push	rbx
-	lea	r10, [r11+r9]
-	mov	rcx, qword [r10]
+	sub	rsi, 1
+	mov	r9, rdi
+	mov	rdi, rdx
+	lea	r8, [rsi*8]
+	lea	r11, [r9+r8]
+	sub	rsp, 8
+	mov	rcx, qword [r11]
+	mov	r10, rcx
+	xor	r10, rdx
+	shr	r10, 63
+	cmp	rdx, -1
+	jne	.L16
+	mov	rax, 0x8000000000000000
+	cmp	rcx, rax
+	jne	.L16
+.L14:
+	mov	rax, 0x7fffffffffffffff
+	mov	rbx, -1
+	jmp	.L2
+.L16:
 	mov	rax, rcx
-	mov	rbx, rcx
-	xor	rbx, rdx
 	cqo
-	idiv	r8
-	lea	rax, [rdx+r8]
+	idiv	rdi
+	lea	rax, [rdx+rdi]
 	cqo
-	idiv	r8
+	idiv	rdi
 	mov	rax, rcx
 	sub	rax, rdx
-	mov	rsi, rdx
+	mov	rbx, rdx
 	cqo
-	idiv	r8
-	mov	qword [r10], rax
-	test	rdi, rdi
-	je	.L2
-	lea	r10, -8[r11+r9]
-	mov	r9d, 1
-.L6:
-	mov	rdi, qword [r10]
-	mov	ecx, 63
-.L5:
-	mov	rax, rdi
-	shr	rax, cl
-	and	eax, 1
-	lea	rax, [rax+rsi*2]
-	mov	rsi, r9
-	cqo
-	sal	rsi, cl
-	idiv	r8
-	mov	rbp, rsi
-	or	rsi, rdi
-	not	rbp
-	and	rbp, rdi
-	mov	rdi, rbp
-	test	rax, rax
-	cmovne	rdi, rsi
-	mov	rsi, rdx
-	sub	ecx, 1
-	jnb	.L5
-	mov	qword [r10], rdi
-	lea	rax, -8[r10]
-	cmp	r11, r10
-	je	.L2
-	mov	r10, rax
-	jmp	.L6
+	idiv	rdi
 .L2:
-	test	rbx, rbx
-	jns	.L1
+	mov	qword [r11], rax
+	lea	rbp, -8[r9+r8]
+	mov	r8d, 1
 	test	rsi, rsi
-	je	.L1
-	sub	rsi, r8
+	je	.L9
 .L8:
-	mov	rax, qword [r11]
-	add	r11, 8
+	mov	rdx, qword 0[rbp]
+	mov	ecx, 63
+	jmp	.L7
+.L17:
+	or	rdx, rsi
+	sub	ecx, 1
+	jb	.L30
+.L7:
+	mov	rax, rdx
+	mov	rsi, r8
+	shr	rax, cl
+	sal	rsi, cl
+	and	eax, 1
+	lea	rax, [rax+rbx*2]
+	mov	rbx, rax
+	sub	rbx, rdi
+	mov	r11, rbx
+	xor	r11, rax
+	jns	.L17
+	cmp	rdi, rax
+	je	.L17
+	not	rsi
+	mov	rbx, rax
+	and	rdx, rsi
+	sub	ecx, 1
+	jnb	.L7
+.L30:
+	mov	qword [rbp], rdx
+	lea	rax, [rbp-8]
+	cmp	r9, rbp
+	je	.L9
+	mov	rbp, rax
+	jmp	.L8
+.L9:
+	test	rbx, rbx
+	je	.L11
+	test	r10b, r10b
+	je	.L11
+	sub	rbx, rdi
+.L12:
+	mov	rax, qword [r9]
+	add	r9, 8
 	add	rax, 1
-	mov	qword -8[r11], rax
+	mov	qword [r9-8], rax
 	test	rax, rax
-	je	.L8
-.L1:
-	mov	rax, rsi
+	je	.L12
+.L11:
+	xor rdx, rdx
+	sub rdi, rbx
+	idiv rdi
+	add	rsp, 8
+	mov	rax, rbx
 	pop	rbx
 	pop	rbp
 	ret
